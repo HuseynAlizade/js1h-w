bc2eb530060abb374926d7ba849175e0d6ac4e83
HuseynAlizade <huseynalizade2010@gmail.com>
2024-08-05 21:56:37 +0400

    js1h-w

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+console.log("Salam");
+//Eror
+console.error("Salam");
+// Warn
+ console.warn("Salam");
+
+//todo Name Surname Age
+
+var name = "Huseyn";
+console.log("name")
+
+var surname = "Alizade";
+console.log("surname")
+
+var age = "14";
+console.log("age")
+
+
+
+//! Enter
+
+var username = prompt("Please enter your name");
+
+var usersurname = prompt("Please enter your surname");
+
+var userage = prompt("Please enter your age");
+
+console.log = (username + " " + usersurname + " " + userage);
